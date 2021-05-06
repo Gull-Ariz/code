e6417ad1125514e0938dbde7f935a0fab69d796e
MuhammadHamzaYousaf <bcsf17a553@pucit.edu.pk>
2021-05-06 13:22:31 +0500

    Image Slider Commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './component/NavbarDropdown/Navbar';
 import Footer from './component/Footer/Footer';
+import ImageSlider from './component/ImageSlider/ImageSlider';
 import {BrowserRouter as Router} from 'react-router-dom'
 function App() {
   return (
     <>
     <Router>
       <Navbar/>
+      <ImageSlider/>
       <Footer/>
     </Router>
     
---
+++ src/component/ImageSlider/ImageSlider.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './ImageSlider.css'
+
+function ImageSlider() {
+    return (
+        <>
+            <div className="Imgdiv"></div>
+        </>
+    )
+}
+export default ImageSlider
